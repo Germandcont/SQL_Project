260ad161dd2d43291a19f81fa1eaff7f42787545
Germán Domínguez <gdominguezcon@gmail.com>
2025-02-23 23:35:36 +0100

    Querys

---
+++ Sql_Querys/calendar.sql
@@ -0,0 +1,26 @@
+-- COMENTO LAS COLUMNAS QUE NO VOY A USAR DE LA TABLA DESDE SQL SERVER PARA QUE NO SE CARGUEN EN POWER BI -- 
+
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day, 
+  --[SpanishDayNameOfWeek], 
+  --[FrenchDayNameOfWeek], 
+  --[DayNumberOfMonth], 
+  --[DayNumberOfYear], 
+  --[WeekNumberOfYear],
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,  
+  --[SpanishMonthName], 
+  --[FrenchMonthName], 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year --[CalendarSemester], 
+  --[FiscalQuarter], 
+  --[FiscalYear], 
+  --[FiscalSemester] 
+FROM 
+ [AdventureWorksDW2022].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2012
